e9339c2ee86d875f7a8d0401f3890c84448d249f
dlozina <dino.lozina@live.com>
2019-08-08 11:57:56 +0200

    testmodel

---
+++ PhotoConverterUI/Model/Photodata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoConverterUI.Model
+{
+    class Photodata
+    {
+        public string image1 { get; set; }
+        public string image2 { get; set; }
+        public string image3 { get; set; }
+        public string image4 { get; set; }
+    }
+}
